2dd19e7919a4635d28af4170ecdd7dd63944be59
ImenGa <88241052+ImenGa@users.noreply.github.com>
2025-01-26 11:05:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ desktop_path = r"C:\Users\etudiant\Desktop\object_tracking_model"
 # Chargement du modèle YOLOv8 pré-entraîné
 model = YOLO("yolov8n.pt") 
 
-# Lancer l'entraînement
+# Lancement de l'entraînement
 train_params = {
     "data": data_yaml_path,    # Chemin vers le fichier .yaml
-    "epochs": 25,           # Nombre d'époques
-    "imgsz": 640,             # Taille des images
-    "batch": 16,              # Taille du batch
-    "name": desktop_path,     # Sauvegarder les résultats sur le Bureau
-    "workers": 4              # Nombre de threads pour le chargeent des données
+    "epochs": 25,          
+    "imgsz": 640,             
+    "batch": 16,              
+    "name": desktop_path,     
+    "workers": 4              
 }
 
 print(f"Entraînement en cours... Les résultats seront sauvegardés dans : {desktop_path}")
 model.train(**train_params)
 
-# Déplacement des résultats vers le Bureau si nécessaire
+# Déplacement des résultats vers le Bureau 
 source_path = f"runs/detect/{desktop_path.split('/')[-1]}"
 shutil.move(source_path, desktop_path)
 print(f"Résultats déplacés vers : {desktop_path}")
@@ -30,7 +30,7 @@ print(f"Résultats déplacés vers : {desktop_path}")
 # Visualisation des graphes
 results_png_path = f"{desktop_path}/results.png"  # Chemin du graphique des performances
 try:
-    img = plt.imread(results_png_path)  # Charger le graphique
+    img = plt.imread(results_png_path)  
     plt.figure(figsize=(10, 8))
     plt.imshow(img)
     plt.axis('off')
